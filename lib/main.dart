ab89fdc386da77184455571ab2bf8d45526e51cf
zuwhi <zuwhyyy@gmail.com>
2024-06-09 21:36:19 +0700

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -7,22 +5,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:plantopia/helpers/fcm_token.dart';
 import 'package:plantopia/service/firebase_messaging_service.dart';
-import 'package:plantopia/service/notification_service.dart';
 import 'package:plantopia/splash_screen_view.dart';
 import 'package:plantopia/utils/app_routes.dart';
 import 'package:plantopia/views/auth/allow_notif_view.dart';
 import 'package:plantopia/views/auth/auth_view.dart';
 import 'package:plantopia/views/global_widgets/bottom_navigation_bar_global_widget.dart';
-import 'package:plantopia/views/home/home_view.dart';
-import 'package:plantopia/views/onboarding/onboarding_view.dart';
 import 'package:plantopia/views/history_plant/history_plant_view.dart';
 import 'package:plantopia/views/home/home_view.dart';
 import 'package:plantopia/views/my_plant/my_plant_view.dart';
+import 'package:plantopia/views/onboarding/onboarding_view.dart';
 import 'package:plantopia/views/weather/weather_view.dart';
 
 import 'firebase_options.dart';
-
-
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
@@ -59,10 +53,8 @@ Future<void> main() async {
           name: AppRoutes.allowNotif,
           page: () => const AllowNotificationView(),
         ),
+        GetPage(name: AppRoutes.onboarding, page: () => const OnboardingView()),
         GetPage(
-          name: AppRoutes.onboarding,
-          page: () => const OnboardingView()),
-       GetPage(
           name: AppRoutes.myPlant,
           page: () => MyPlantView(),
         ),
